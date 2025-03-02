1fca22fce4d154cd8e34c771edb3ce51e856ad5c
sourabhpramod <psourabh2@gmail.com>
2025-03-02 15:26:35 +0530

    waiting room and sockets done, networking fully done, room code generation and entering to mainscene

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,17 +1,54 @@
+const gameRooms = {
+  // [roomKey]: {
+  // users: [],
+  // randomTasks: [],
+  // scores: [],
+  // gameScore: 0,
+  // players: {},
+  // numPlayers: 0
+  // }
+};
+
+
 module.exports = (io) => {
-    io.on("connection", (socket) => {
-      console.log("A user connected");
-  
-      socket.on("disconnect", () => {
-        console.log("A user disconnected");
-      });
-  
-      // Add your Socket.IO event handlers here
-      socket.on("chat message", (msg) => {
-        io.emit("chat message", msg); // Broadcast message to all connected clients
-      });
+  io.on("connection", (socket) => {
+    console.log(
+      `A socket connection to the server has been made: ${socket.id}`
+    );
+
+    socket.on("joinRoom", (roomKey)=>{
+      socket.join(roomKey);
     });
-  };
 
+    socket.on("isKeyValid", function (input) {
+      const keyArray = Object.keys(gameRooms);
+      keyArray
+        ? socket.emit("keyIsValid", input)
+        : socket.emit("keyNotValid");
+    });
+    socket.on("getRoomCode", async function () {
+      let key = codeGenerator();
+      Object.keys(gameRooms).includes(key) ? (key = codeGenerator()) : key;
+      gameRooms[key] = {
+        roomKey: key,
+        randomTasks: [],
+        gameScore: 0,
+        scores: {},
+        players: {},
+        numPlayers: 0,
+      };
+      socket.emit("roomCreated", key);
+    });
+  });
 
-  //DUMMY FILE RUN CHEYYAN VENDI ADD AAKKIYATHA
+  function codeGenerator() {
+    let code = "";
+    let chars = "ABCDEFGHJKLMNPQRSTUVWXYZ0123456789";
+    for (let i = 0; i < 5; i++) {
+      code += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return code;
+  }
+
+};
+
--- src/scenes/Mainscene.js
+++ src/scenes/Mainscene.js
@@ -4,18 +4,20 @@ export default class MainScene extends Phaser.Scene{
         super("MainScene");
     }
     preload(){
-        // this.load.spritesheet("character", "assets/spritesheets/character1.png",{
-        //     frameWidth: 29,
-        //     frameHeight: 37,
-        // });
+        this.load.spritesheet("character", "assets/spritesheets/character1.png",{
+             frameWidth: 29,
+             frameHeight: 37,
+        });
         this.load.image("mainroom", "assets/backgrounds/background.png");
     }
     create(){
         const scene = this;
-        this.add.image(0,0,"mainroom").setOrigin(0);
+        let bg = this.add.image(0,0,"mainroom").setOrigin(0);
+        bg.scale=1.25;
         this.socket=io();
+        scene.scene.launch("WaitingRoom", {socket: scene.socket});
     }
     update(){
-
+        
     }
 }
