e2f0f0c16e5e5f75cbbd3fc95e40eed5d21ebd3c
sourabhpramod <psourabh2@gmail.com>
2025-03-02 17:37:02 +0530

    player added to the scene and socket networking handled

--- server/socket/index.js
+++ server/socket/index.js
@@ -17,7 +17,39 @@ module.exports = (io) => {
     );
 
     socket.on("joinRoom", (roomKey)=>{
+
+      if (!gameRooms[roomKey]) {
+        gameRooms[roomKey] = {
+          roomKey,
+          randomTasks: [],
+          gameScore: 0,
+          scores: {},
+          players: {},
+          numPlayers: 0,
+        };
+      }
+     
       socket.join(roomKey);
+      const roomInfo = gameRooms[roomKey];
+      console.log("roomInfo", roomInfo);
+      roomInfo.players[socket.id]={
+        rotation: 0,
+        x: 400,
+        y: 300,
+        playerid: socket.id,
+      };
+      roomInfo.numPlayers = Object.keys(roomInfo.players).length;
+      socket.emit("setState", roomInfo);
+
+      io.to(roomKey).emit("currentPlayers", {
+        players: roomInfo.players,
+        numPlayers: roomInfo.numPlayers,
+      });
+      
+      socket.to(roomKey).emit("newPlayer", {
+        playerInfo: roomInfo.players[socket.id],
+        numPlayers: roomInfo.numPlayers,
+      });
     });
 
     socket.on("isKeyValid", function (input) {
--- src/scenes/Mainscene.js
+++ src/scenes/Mainscene.js
@@ -2,21 +2,60 @@ import Phaser from "phaser";
 export default class MainScene extends Phaser.Scene{
     constructor(){
         super("MainScene");
+        this.state={};
     }
     preload(){
-        this.load.spritesheet("character", "assets/spritesheets/character1.png",{
-             frameWidth: 29,
-             frameHeight: 37,
-        });
+        this.load.image("character", "assets/spritesheets/character1.png");
+
         this.load.image("mainroom", "assets/backgrounds/background.png");
     }
     create(){
         const scene = this;
         let bg = this.add.image(0,0,"mainroom").setOrigin(0);
-        bg.scale=1.25;
+        bg.setScale(0.2);       
         this.socket=io();
         scene.scene.launch("WaitingRoom", {socket: scene.socket});
+        this.otherPlayers = this.physics.add.group();
+
+        this.socket.on("setState", function(state){
+            const {roomKey, players, numPlayers} = state;
+            scene.physics.resume();
+            scene.state.roomKey = roomKey;
+            scene.state.players = players;
+            scene.state.numPlayers = numPlayers;
+
+        });
+        this.socket.on("currentPlayers", function(args){
+            const { players, numPlayers} = args;
+            scene.state.numPlayers = numPlayers;
+            Object.keys(players).forEach(function (id){
+                if(players[id].playerid === scene.socket.id){
+                    scene.addPlayer(scene, players[id]);
+                }else{
+                    scene.addOtherPlayers(scene, players[id]);
+                }
+            });
+        });
+
+        this.socket.on("newPlayer", function(arg){
+            const { playerInfo, numPlayers } = arg;
+            scene.addOtherPlayers(scene, playerInfo);
+            scene.state.numPlayers = numPlayers;
+        });
     }
+
+    addPlayer(scene, playerInfo){
+        scene.joined = true;
+        scene.character = scene.physics.add.sprite(
+            playerInfo.x, playerInfo.y, "character"
+        ).setOrigin(0.5,0.5).setSize(30,40).setOffset(0,24);
+    }
+    addOtherPlayers(scene, playerInfo){
+        const otherPlayer = scene.add.sprite(playerInfo.x + 40, playerInfo.y + 40, "character");
+        otherPlayer.playerId = playerInfo.playerId;
+        scene.otherPlayers.add(otherPlayer);
+    }
+
     update(){
         
     }
