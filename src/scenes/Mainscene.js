0abf5e88315ecea12216f1c5ec705db6b6d0ab61
sourabhpramod <psourabh2@gmail.com>
2025-03-04 20:46:05 +0530

    Player movement using arrow keys done

--- src/scenes/Mainscene.js
+++ src/scenes/Mainscene.js
@@ -1,46 +1,51 @@
 import Phaser from "phaser";
-export default class MainScene extends Phaser.Scene{
-    constructor(){
+
+export default class MainScene extends Phaser.Scene {
+    constructor() {
         super("MainScene");
-        this.state={};
+        this.state = {};
     }
-    preload(){
+
+    preload() {
         this.load.spritesheet("character", "assets/spritesheets/character1.png", {
-            frameWidth: 896,   // Width of each frame in the sheet
-            frameHeight: 896   // Height of each frame in the sheet
+            frameWidth: 896,  
+            frameHeight: 896   
         });
         this.load.image("mainroom", "assets/backgrounds/background.png");
     }
-    create(){
-        const scene = this;
-        let bg = this.add.image(0,0,"mainroom").setOrigin(0);
-        bg.setScale(0.2);       
-        
-        this.socket=io();
-        scene.scene.launch("WaitingRoom", {socket: scene.socket});
-        this.otherPlayers = this.physics.add.group();
 
-        this.socket.on("setState", function(state){
-            const {roomKey, players, numPlayers} = state;
+    create() {
+        const scene = this;
+        let bg = this.add.image(0, 0, "mainroom").setOrigin(0);
+        bg.setScale(0.2);
+
+        this.socket = io();
+        scene.scene.launch("WaitingRoom", { socket: scene.socket });
+
+        this.otherPlayers = this.physics.add.group();
+        this.cursors = this.input.keyboard.createCursorKeys(); // Arrow key input
+
+        this.socket.on("setState", function (state) {
+            const { roomKey, players, numPlayers } = state;
             scene.physics.resume();
             scene.state.roomKey = roomKey;
             scene.state.players = players;
             scene.state.numPlayers = numPlayers;
-
         });
-        this.socket.on("currentPlayers", function(args){
-            const { players, numPlayers} = args;
+
+        this.socket.on("currentPlayers", function (args) {
+            const { players, numPlayers } = args;
             scene.state.numPlayers = numPlayers;
-            Object.keys(players).forEach(function (id){
-                if(players[id].playerid === scene.socket.id){
+            Object.keys(players).forEach(function (id) {
+                if (players[id].playerid === scene.socket.id) {
                     scene.addPlayer(scene, players[id]);
-                }else{
+                } else {
                     scene.addOtherPlayers(scene, players[id]);
                 }
             });
         });
 
-        this.socket.on("newPlayer", function(arg){
+        this.socket.on("newPlayer", function (arg) {
             const { playerInfo, numPlayers } = arg;
             scene.addOtherPlayers(scene, playerInfo);
             scene.state.numPlayers = numPlayers;
@@ -52,8 +57,11 @@ export default class MainScene extends Phaser.Scene{
         scene.character = scene.physics.add.sprite(
             playerInfo.x, playerInfo.y, "character", 0  
         ).setOrigin(0.5, 0.5).setSize(30, 40).setOffset(0, 24).setScale(0.1);
+
+        scene.character.setCollideWorldBounds(true); // Prevent moving outside the screen
+        scene.physics.world.setBounds(0, 0, 800, 600); // Set world boundaries
     }
-    
+
     addOtherPlayers(scene, playerInfo) {
         const otherPlayer = scene.add.sprite(
             playerInfo.x + 40, playerInfo.y + 40, "character", 0  
@@ -62,7 +70,24 @@ export default class MainScene extends Phaser.Scene{
         scene.otherPlayers.add(otherPlayer);
     }
 
-    update(){
+    update() {
+        if (!this.joined || !this.character) return; // Prevent errors before joining
+
+        const speed = 200;
+        this.character.setVelocity(0); // Reset velocity before applying movement
+
+        if (this.cursors.left.isDown) {
+            this.character.setVelocityX(-speed);
+        } else if (this.cursors.right.isDown) {
+            this.character.setVelocityX(speed);
+        }
+
+        if (this.cursors.up.isDown) {
+            this.character.setVelocityY(-speed);
+        } else if (this.cursors.down.isDown) {
+            this.character.setVelocityY(speed);
+        }
+
         
     }
 }
